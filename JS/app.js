06e1fdfe3094a86500dac7d302d0b2f37fec4655
snehaISL <sneha.sural111@gmail.com>
2022-10-12 18:49:22 +0530

    coupon

--- JS/app.js
+++ JS/app.js
@@ -4,3 +4,15 @@ $(function() {
       $('#nav-search').find('.nav-search-label').html(selectedText);
     });
   });
+  function loadCoupon(){
+    document.getElementById('coupon').style.visibility = 'visible';
+    document.getElementById('promo-carousel').style.opacity='0.9'
+}
+
+function closeCoupon(){
+    document.getElementById('coupon').style.visibility = 'hidden';
+    document.getElementById('promo-carousel').style.opacity='1'
+}
+
+
+
